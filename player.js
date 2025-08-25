e27a2905c6d332055df45cfc44cf805ab9d09ad2
tbrookx <83836595+tbrookx@users.noreply.github.com>
2025-08-25 15:55:58 -0700

    Add files via upload

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 async function loadPlaylist() {
-  const repo = "tbrookx/betap-layer"; // replace with your GitHub username/repo
+  const repo = "tbrookx/beta-player"; // replace with your GitHub username/repo
   const branch = "main"; // or 'master'
   const apiUrl = `https://api.github.com/repos/${repo}/contents/music?ref=${branch}`;
   const response = await fetch(apiUrl);
